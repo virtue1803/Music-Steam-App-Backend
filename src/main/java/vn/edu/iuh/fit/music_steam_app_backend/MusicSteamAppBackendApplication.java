a4e95e02f6f2b214bd66e01bd429bcec5c4086cd
virtue1803 <nguyenductai505@gmail.com>
2024-11-19 08:34:16 +0700

    init the project

---
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/MusicSteamAppBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.music_steam_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSteamAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicSteamAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/music_steam_app_backend/MusicSteamAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.music_steam_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSteamAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
