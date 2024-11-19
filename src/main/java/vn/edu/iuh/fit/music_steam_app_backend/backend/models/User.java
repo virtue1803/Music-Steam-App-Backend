a2fa33e0ba3d2a7532f1ca81bffea7c33b685e0f
virtue1803 <nguyenductai505@gmail.com>
2024-11-19 08:36:43 +0700

    User management (login name, password, role, email, etc.).

---
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/models/User.java
@@ -0,0 +1,32 @@
+package vn.edu.iuh.fit.music_steam_app_backend.backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import vn.edu.iuh.fit.music_steam_app_backend.backend.enums.Role;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NonNull
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @NonNull
+    @Column(name = "role", nullable = false)
+    private Role role; // Enum liên kết tới vai trò người dùng
+}
