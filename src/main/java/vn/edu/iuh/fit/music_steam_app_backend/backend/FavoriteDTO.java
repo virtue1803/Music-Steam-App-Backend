4bac30064f4bac581343ddf4085687b74a9237e0
virtue1803 <nguyenductai505@gmail.com>
2024-11-19 08:41:04 +0700

    DTO for the user's favorite song.

---
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/FavoriteDTO.java
@@ -0,0 +1,12 @@
+package vn.edu.iuh.fit.music_steam_app_backend.backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FavoriteDTO {
+    private Long userId;
+    private Long songId;
+    private String addedAt; // Date when the song was added to favorites
+}
