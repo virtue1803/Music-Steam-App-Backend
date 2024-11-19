fd8acb8447dfba4c26fcf1ec8eb9d16203372712
virtue1803 <nguyenductai505@gmail.com>
2024-11-19 08:39:37 +0700

    Music genre

---
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/enums/Genre.java
@@ -0,0 +1,29 @@
+package vn.edu.iuh.fit.music_steam_app_backend.backend.enums;
+
+public enum Genre {
+    POP,
+    ROCK,
+    RAP,
+    JAZZ,
+    BLUES,
+    COUNTRY,
+    CLASSICAL,
+    ELECTRONIC,
+    DANCE,
+    HIP_HOP,
+    R_AND_B,
+    REGGAE,
+    FOLK,
+    LATIN,
+    METAL,
+    PUNK,
+    SOUL,
+    FUNK,
+    DISCO,
+    REGGAETON,
+    INSTRUMENTAL,
+    K_POP,
+    J_POP,
+    V_POP,
+    OTHER
+}
