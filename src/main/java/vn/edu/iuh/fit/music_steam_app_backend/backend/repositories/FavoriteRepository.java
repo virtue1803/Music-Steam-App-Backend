d11ebdab3b9c8172c9bf7fd0b3623dd95067787c
virtue1803 <nguyenductai505@gmail.com>
2024-11-24 19:47:46 +0700

    update service Favorite and SongPlaylist

--- src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/repositories/FavoriteRepository.java
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/repositories/FavoriteRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.music_steam_app_backend.backend.ids.FavoriteId;
 import vn.edu.iuh.fit.music_steam_app_backend.backend.models.Favorite;
+
+import java.util.Optional;
+
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
+
+    Optional<Favorite> findById(Long id);
 }
--- src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/repositories/SongPlaylistRepository.java
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/backend/repositories/SongPlaylistRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.music_steam_app_backend.backend.ids.SongPlaylistId;
 import vn.edu.iuh.fit.music_steam_app_backend.backend.models.SongPlaylist;
+
+import java.util.Optional;
+
 @Repository
 public interface SongPlaylistRepository extends JpaRepository<SongPlaylist, SongPlaylistId> {
+
+
+    Optional<SongPlaylist> findById(Long id);
 }
