51e312fef360e2f00c5418f594b4354a188af708
virtue1803 <nguyenductai505@gmail.com>
2024-12-04 23:53:03 +0700

    implements CommandLineRunner in main class

--- src/main/java/vn/edu/iuh/fit/music_steam_app_backend/MusicSteamAppBackendApplication.java
+++ src/main/java/vn/edu/iuh/fit/music_steam_app_backend/MusicSteamAppBackendApplication.java
@@ -1,13 +1,18 @@
 package vn.edu.iuh.fit.music_steam_app_backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusicSteamAppBackendApplication {
+public class MusicSteamAppBackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicSteamAppBackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
